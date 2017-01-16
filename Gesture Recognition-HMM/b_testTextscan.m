68121955eddfd0532d366b077e2d1b032acac1f0
Jane <459333694@qq.com>
2017-01-16 19:50:27 +0900

    Add files via upload

---
+++ Gesture Recognition-HMM/b_testTextscan.m
@@ -0,0 +1,11 @@
+tagstream_file = 'P3_2_8_p29.tagstream'
+fid=fopen(tagstream_file,'r');
+ disp(fid)
+ disp('holla');
+B = fread(fid,'*char')';
+disp(B);
+A=textscan(B,'%s %s','delimiter',';')
+A{1}
+A{2}
+%A = textscan(zzA,'%s','delimiter','\n')
+fclose(fid);
